Fixing batch normalisation mean and variance transfer

CNNlearner.py
@@ -17,6 +17,9 @@ class CNNLearner(nn.Module):
         self.batch_norm_2 = nn.BatchNorm2d(n_filters, eps=1e-03, momentum=bn_momentum)
         self.batch_norm_3 = nn.BatchNorm2d(n_filters, eps=1e-03, momentum=bn_momentum)
 
+        # Used for easy access to the layers
+        self.bn_list = [self.batch_norm_0, self.batch_norm_1, self.batch_norm_2, self.batch_norm_3]
+
         fc_in = image_size // 2**4
         self.fc = nn.Linear(fc_in * fc_in * n_filters, output_dim)
 
@@ -57,10 +60,23 @@ class CNNLearner(nn.Module):
         """
         return torch.cat([p.view(-1) for p in self.parameters()], 0)
 
-    def replace_flat_params(self, flat_params):
+    def get_bn_stats(self):
         """
-        Copies the weight value of the input params to the parameters of the learner
+        :return: Tuple containing the batch normalisation layers running stats.
+        """
+        bn_means, bn_vars = [], []
+        for layer in self.bn_list:
+            bn_means.append(layer.running_mean)
+            bn_vars.append(layer.running_var)
+        return bn_means, bn_vars
+
+    def replace_flat_params(self, flat_params, bn_stats=None):
+        """
+        Copies the weight value of the input params to the parameters of the learner.
+        Copies the batch normalisation layers running stats
         :param flat_params: flattened tensor of len(n_learner_params)
+        :param bn_means: list of tensors of len(bn_list)
+        :param bn_vars: list of tensors len(bn_list)
         """
         current_position = 0
         for param in self.parameters():
@@ -68,3 +84,9 @@ class CNNLearner(nn.Module):
             corresponding_weights = flat_params[current_position: current_position + num_weights].view_as(param)
             param.data.copy_(corresponding_weights)
             current_position += num_weights
+
+        # Copy the batch norm running means and variances if needed
+        if bn_stats is not None:
+            for idx, layer in enumerate(self.bn_list):
+                layer.running_mean = bn_stats[0][idx]
+                layer.running_var = bn_stats[1][idx]

train_meta_learner.py
@@ -137,9 +137,10 @@ def meta_test(val_dataset, learner, learner_wo_grad, metalearner):
         learner.train()
         learner_wo_grad.eval()
 
-        # Get and copy the updated parameters for the learner
+        # Get and copy the updated parameters for the learner as well as the running stats of the other learner
         cell_state = train_learner(learner, metalearner, train_x, train_y)
-        learner_wo_grad.replace_flat_params(cell_state)
+        stats = learner.get_bn_stats()
+        learner_wo_grad.replace_flat_params(cell_state, stats)
 
         # Get validation set predictions and return accuracy
         output = learner_wo_grad(test_x)
@@ -200,8 +201,10 @@ def main():
         grad_free_learner.train()
         new_cell_state = train_learner(learner, metalearner, train_x, train_y)
 
-        # new cell state contains our parameters in a 1-d array
-        grad_free_learner.replace_flat_params(new_cell_state)
+        # new cell state contains our parameters in a 1-d array,
+        # we trained using the other learner so we want to keep its running stats as well
+        stats = learner.get_bn_stats()
+        grad_free_learner.replace_flat_params(new_cell_state, stats)
 
         output = grad_free_learner(test_x)
         predictions = torch.max(output[:], 1)[1]