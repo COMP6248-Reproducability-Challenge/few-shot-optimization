c6de012601d0d182069233ed280505acdbe9edeb
AranBSmith <aran.smith47@mail.dcu.ie>
2019-05-13 21:33:21 +0100

    created scripts for 10 shot learning

--- scripts/10_shot_MNIST_test.sh
+++ scripts/10_shot_MNIST_test.sh
@@ -3,9 +3,10 @@ python3 -u train_meta_learner.py \
     -its 10000\
     -cuda 0 \
     -val 1000 \
-    -data MIN \
+    -test \
+    -data MNIST \
     -classes 10 \
-    -data_root ../data/miniImagenet/ \
+    -data_root ../data/ \
+    -load_state mings10class10shot \
     -channel gscale \
-    -save_state mings10class10shot \
     -shots 10
